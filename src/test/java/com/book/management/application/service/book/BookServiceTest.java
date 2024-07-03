d82c296ee554fdda32a42b1ecc120b4577e717f7
Abdurashid Vakhobov <avcoding01@gmail.com>
2024-07-03 21:57:02 +0500

    Refactors

--- src/test/java/com/book/management/application/service/book/BookServiceTest.java
+++ src/test/java/com/book/management/application/service/book/BookServiceTest.java
@@ -5,12 +5,14 @@ import com.book.management.application.model.BookModel;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(classes = {ManagementApplication.class})
+@ActiveProfiles("test")
 public class BookServiceTest {
 
     @Autowired
